6d5e17a694e81e11fc43300945d17e2eeb942366
shigural <shigural.ae@gmail.com>
2021-03-28 15:58:04 +0900

    自走期から撮影した画像に差し替え。プログラムを微修正

--- SSC-ImageRecognition/SSC-ImageRecognition/SSC_ImageRecognition.py
+++ SSC-ImageRecognition/SSC-ImageRecognition/SSC_ImageRecognition.py
@@ -4,20 +4,19 @@ from matplotlib import pyplot as plt
 import os
 import math
 import glob
+import time
 
+start = time.time()
 path="D:\Programing\SSR\SSC-ImageRecognition\SSC-ImageRecognition\\"
 cable_size=22
 circle_diameter=82
 
-
-print(glob.glob(path+'Image\\*.jpg'))
-for data in glob.glob(path+'Image\\*.jpg'):
+for data in glob.glob(path+'Image\\*.png'):
     img = cv2.imread(data)
     original_img=img
     height, width, channels = img.shape[:3]
 
     loop_count=50
-   
     B=0
     G=0
     R=0
@@ -31,15 +30,11 @@ for data in glob.glob(path+'Image\\*.jpg'):
     G/=loop_count
     R/=loop_count
     
-
-
-
-
     print(B,G,R)
     min_rate=0.5
     max_rate=2
     color_range=40
-#[174+25, 119+25, 20+25])
+
     min_B=max(0,B-color_range)
     min_G=max(0,G-color_range)
     min_R=max(0,R-color_range)
@@ -47,11 +42,7 @@ for data in glob.glob(path+'Image\\*.jpg'):
     max_G=min(255,G+color_range)
     max_R=min(255,R+color_range)
     
-
-    print([min_B,min_G,min_R])
-    print([max_B,max_G,max_R])
-    bgrLower = np.array([min_B,min_G,min_R])    # 抽出する色の下限(BGR)
-    bgrUpper = np.array([max_B,max_G,max_R])    # 抽出する色の上限(BGR)
+    print([min_B,min_G,min_R],[max_B,max_G,max_R])
     bgrLower = np.array([min_B,min_G,min_R])    # 抽出する色の下限(BGR)
     bgrUpper = np.array([max_B,max_G,max_R])    # 抽出する色の上限(BGR)
     img_mask = cv2.inRange(img, bgrLower, bgrUpper) # BGRからマスクを作成
@@ -75,12 +66,15 @@ for data in glob.glob(path+'Image\\*.jpg'):
         if(black_counter>BLACK_RESETPOINT):
             start_point=0
             black_counter=0
+
     print(start_point)
 
-    result_img = cv2.circle(original_img,( math.floor(width/2),start_point+math.floor(circle_diameter/2)), 5, (0,0,255), -1)
-
+    if(start_point>circle_diameter): result_img = cv2.circle(original_img,( math.floor(width/2),start_point+math.floor(circle_diameter/2)), 5, (0,0,255), -1)
+    else :result_img=original_img
 
     cv2.imwrite(path+"result\\"+os.path.basename(data),result_img)
-    cv2.imshow("img_th", result_img)
+    cv2.imshow("img", result_img)
     cv2.waitKey()
     cv2.destroyAllWindows()
+print ("elapsed_time:{0}".format( time.time() - start) + "[sec]")
+
