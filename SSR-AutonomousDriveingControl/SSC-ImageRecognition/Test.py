9d719ff01e4474aa3c49f103fbfa196b2a375c8f
shigural <30390718+shigural@users.noreply.github.com>
2022-04-20 16:39:34 +0900

    領域拡張法でレンジと繋がり方を限定したものを実装し、有効性を確認

--- SSR-AutonomousDriveingControl/SSC-ImageRecognition/Test.py
+++ SSR-AutonomousDriveingControl/SSC-ImageRecognition/Test.py
@@ -1,4 +1,4 @@
 import cv2 #pip install opencv-python
-from SSC_ImageRecognition9 import IRRecognition
+from SSC_ImageRecognition10 import IRRecognition
 ir_image=cv2.imread('ir.png')
 IRRecognition(ir_image)
